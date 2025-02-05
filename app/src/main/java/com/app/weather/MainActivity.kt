6bfa132303b40a05574786b133f9ddbb0bd9ca93
yshanmug <yuvi.auroraborials@gmail.com>
2025-02-04 22:29:55 -0500

    Update top padding.

--- app/src/main/java/com/app/weather/MainActivity.kt
+++ app/src/main/java/com/app/weather/MainActivity.kt
@@ -82,7 +82,7 @@ fun WeatherContent(weatherState: WeatherResponseDto?, modifier: Modifier = Modif
             Text(
                 text = "Hourly Weather Data.",
                 style = MaterialTheme.typography.headlineSmall,
-                modifier = Modifier.padding(bottom = 16.dp, top = 20.dp)
+                modifier = Modifier.padding(bottom = 16.dp, top = 30.dp)
             )
 
         }
